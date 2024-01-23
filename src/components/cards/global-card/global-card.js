0c7026f018959a3780fb6330b17c8cee191626c5
leyinko <co.giro.luca@gmail.com>
2024-01-23 12:09:27 +0100

    Second one click test iPad

--- src/components/cards/global-card/global-card.js
+++ src/components/cards/global-card/global-card.js
@@ -26,7 +26,7 @@ export const globalCardHandlers = () => {
       // Play btn
       let play = e.target;
       // Phone & Tablets
-      if (window.innerWidth < 900 && play) {
+      if (window.innerWidth < 1100 && play) {
         // Handle Play
         await playCard(e);
         // Selected / Playing
